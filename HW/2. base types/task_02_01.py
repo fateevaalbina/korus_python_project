18674dfa673e852ddbbfe899005e52b5dfd7bc93
AFateeva <afateeva@korusconsulting.ru>
2023-02-19 15:47:46 +0300

    add 2.1

---
+++ HW/2. base types/task_02_01.py
@@ -0,0 +1,14 @@
+from fractions import Fraction as fr
+
+aa = input("Введите первую дробь в формате a/b")
+ab = input ("Введите вторую дробь в формате c/d")
+
+if aa.find('/') == True and ab.find('/') == True:
+    ba = int(aa.split('/')[-1])
+    bb = int(ab.split('/')[-1])
+    if (ba and bb) !=0:
+        print(fr(aa) + fr(ab))
+    else:
+        print('Деление на 0')
+else:
+    print('Error. Не верный формат')
