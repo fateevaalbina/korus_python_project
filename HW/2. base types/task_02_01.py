de9ff3ec67cb241923913e7bdb404dbe640239be
AFateeva <afateeva@korusconsulting.ru>
2023-02-19 20:44:20 +0300

    Fixed if

--- HW/2. base types/task_02_01.py
+++ HW/2. base types/task_02_01.py
@@ -3,7 +3,7 @@ from fractions import Fraction as fr
 aa = input("Введите первую дробь в формате a/b")
 ab = input("Введите вторую дробь в формате c/d")
 
-if aa.find('/') is True and ab.find('/') is True:
+if aa.find('/') and ab.find('/') != -1:
     ba = int(aa.split('/')[-1])
     bb = int(ab.split('/')[-1])
     if (ba and bb) != 0:
